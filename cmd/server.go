237ab876d58bf311ff665c4622b37635478c8cbc
erzashala <es686862@ubt-uni.net>
2025-04-01 12:42:40 +0200

    add fhir endpoint

--- cmd/server.go
+++ cmd/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"hl7-fhir-parser/internal/errors"
 	"hl7-fhir-parser/internal/parser"
+	"io/ioutil"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,24 +12,42 @@ func main() {
 	r := gin.Default()
 
 	r.POST("/parse/hl7", func(c *gin.Context) {
-		var message string
-		if err := c.BindJSON(&message); err != nil {
-			// Handle the error using the HandleError function
+		// Read the raw HL7 message from the request body
+		message, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
 			errorResponse, statusCode := errors.HandleError(err)
 			c.JSON(statusCode, errorResponse)
 			return
 		}
 
 		// Parse the HL7 message
-		patient, err := parser.ParseHL7Message(message)
+		patient, err := parser.ParseHL7Message(string(message))
 		if err != nil {
-			// Handle the error using the HandleError function
 			errorResponse, statusCode := errors.HandleError(err)
 			c.JSON(statusCode, errorResponse)
 			return
 		}
 
-		// If the parsing succeeds, return the patient data
+		// Return the parsed patient data as JSON
+		c.JSON(200, patient)
+	})
+
+	// Endpoint to Parse FHIR Messages**
+	r.POST("/parse/fhir", func(c *gin.Context) {
+		message, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			errorResponse, statusCode := errors.HandleError(err)
+			c.JSON(statusCode, errorResponse)
+			return
+		}
+
+		patient, err := parser.ParseFHIRMessage(string(message))
+		if err != nil {
+			errorResponse, statusCode := errors.HandleError(err)
+			c.JSON(statusCode, errorResponse)
+			return
+		}
+
 		c.JSON(200, patient)
 	})
 
